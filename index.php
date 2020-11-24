afdda45cdcdd4ab54837fa6844ece419969107f6
Wara Massa <waramassa70@gmail.com>
2020-11-24 23:18:55 +0100

    php3 exo3

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    $months = array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "décembre");
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP3 exo3</title>
+</head>
+<body>
+        <?php
+        // for ($numero = 0; $numero <= 11; $numero++)
+        // {
+        //   echo $months[$numero] ; //affichera $ $months [0], $months[1] ect...
+        //  }
+
+        // $counter=0;
+
+        // while($counter <= 11) {
+        //    echo $months[$counter];
+        //    $counter++;
+        //  }
+        foreach ($months as $element)
+        ?>
+        
+</body>
+</html>
